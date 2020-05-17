5d3391320e8fb7a2bc007ec1bc4270fd3a3721c8
Davi <davimargelo@hotmail.com>
2020-05-17 14:52:08 -0300

    ex 7

---
+++ ex7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd algumdiretorio
+if [ $? -eq 0 ];then 
+	echo $? 
+	echo "cd executado com sucesso!" 
+else 
+    mkdir algumdiretorio
+    echo "mkdir executado com sucesso!" 
+fi
